9e64128c2a28379722f1a21f87e141f9e127d24c
Anwarlalindunyo <73191749+Anwarlalindunyo@users.noreply.github.com>
2021-01-28 12:16:15 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DGBeQ4sAoq7PuyKYYKNSM2PtMpUwDFm67i -p c=DOGE
+
+./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u  MG8bifgjBjBL7xo5V6GhLD7XXiqNUx8yAU -p c=LTC
 
 
 done
